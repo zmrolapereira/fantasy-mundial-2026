56c105d69c18db36560de1e65022466f35118d91
zmrolapereira <zmrolapereira@gmail.com>
2026-04-29 19:11:17 +0100

    update

---
+++ app/sitemap.ts
@@ -0,0 +1,20 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.fantasymundial2026.com",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.fantasymundial2026.com/login",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.fantasymundial2026.com/team",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.fantasymundial2026.com/ranking",
+      lastModified: new Date(),
+    },
+  ];
+}
